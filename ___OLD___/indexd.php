4dec072a0e06cc67f65febea4dd0a2b581b17bbf
Shedou <chimbal.chimbal@yandex.ru>
2017-11-06 18:41:19 +0300

    initial commit

---
+++ ___OLD___/indexd.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="/img/favicon.gif" type="image/gif">
+
+<title>Hello bich</title>
+<link href="/css/css.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="header"><div class="colort">FCK THIS PAE</div></div>
+<div class="content"><div class="colort">FCK THIS PAE</div>
+
+<video src="WaifuHouse.mp4" controls></video>
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Farmling</title>
+	<link rel="icon" href="/core/favicon.ico" type="image/x-icon">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="core/css/css.css">
+</head>
+<body>
+
+<span style="">Добро пожаловать!</span>
+
+</body>
+</html>
