9077357917a27b3a773081138c5ee4d7d0225dc7
Shedou <chimbal.chimbal@yandex.ru>
2017-11-11 22:33:14 +0300

    ref

--- game/js/canvas.js
+++ game/js/canvas.js
@@ -4,26 +4,23 @@
 let canvasWidth = 1280;
 let canvasHeight = 720;
 
-var canvas = document.getElementById("farmlingViewport");
+let canvas = document.getElementById("farmlingViewport");
 	canvas.setAttribute('width', canvasWidth);
 	canvas.setAttribute('height', canvasHeight);
+let context = canvas.getContext("2d");
 
-var context = canvas.getContext("2d");
 	context.fillStyle = "rgb(0, 162, 232)";
-	context.fillRect(555, 467, 150, 120);
+	context.fillRect(320, 32, 928, 656);
 
-	context.beginPath();
-	context.fillStyle = "rgb(0, 55, 55)";
-	context.arc(423, 93, 70, 0, 2 * Math.PI, true);
-	context.fill();
 
 	context.font = '24px "Arial"';
 	context.fillStyle = "rgba(0, 55, 55, 0.2)";
-	context.fillText("Hello To Farmling!", 540, 600);
+	context.fillText("Hello To Farmling!", 542, 24);
 
-	let imgPlayer= new Image();
-	imgPlayer.src= "game/img/player.png";
+
+	let imgPlayer = new Image();
+	imgPlayer.src = "game/img/player.png";
 
 	imgPlayer.onload = function(){
-	context.drawImage(imgPlayer, 20, 80, 256, 512);
+	context.drawImage(imgPlayer, 32, 104, 256, 512);
 	};
